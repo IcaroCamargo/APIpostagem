569146030bddd3968f6eb8a943c0b3d9e27bd7db
IcaroCamargo <icarod331@gmail.com.br>
2025-11-24 22:30:11 -0300

    GetPostagem

---
+++ APIpostagems/Services/PostagensServices.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using APIpostagems.Models;
+using Newtonsoft.Json;
+
+namespace APIpostagems.Services
+{
+    internal class PostagensServices
+    {
+        public async Task<Postagem> GetAllPostagem()
+            {
+                Postagem minhaPostagem = new Postagem();
+                string URI = "https://jsonplaceholder.typicode.com/posts";
+                HttpClient httpClient = new HttpClient();
+                HttpResponseMessage responseMessage = await httpClient.GetAsync(URI);
+
+            List<Postagem> postagens = new List<Postagem>();
+
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    string conteudo = await responseMessage.Content.ReadAsStringAsync();
+
+                    postagens = JsonConvert.DeserializeObject<List<Postagem>>(conteudo);
+                    
+
+
+                }
+            return postagens[0];
+             }
+    }
+}
--- APIpostagems/ViewModels/PostagensViewModels.cs
+++ APIpostagems/ViewModels/PostagensViewModels.cs
@@ -7,14 +7,20 @@ using System.Threading.Tasks;
 
 namespace APIpostagems.ViewModels
 {
-    public class PostagensViewModels : ObservableObject
+    public partial class PostagensViewModels : ObservableObject
     {
         [ObservableProperty]
         public int userId;
+        [ObservableProperty]
         int id;
+        [ObservableProperty]
         string title;
+        [ObservableProperty]
         string body;
         
+
+        //Método que busca as informações
+        //"Exibir na tela"
 
     }
 }
